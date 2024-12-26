6fd93ed2fb233514b24d8eedcb581b23fae93878
moohammedddd <midorafat115566@gmail.com>
2024-12-26 03:03:58 +0200

    Add project files.

---
+++ Assignment 06/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 01 Explain the difference between passing (Reference type parameters) by value and by reference then write a suitable c# example.
+            /* In Passing by Value in Reference Type it passed the Address of data 
+             * that give the function access of data 
+             * In passing by value it be (Input)
+             * In Passing by Reference it passed the real variable and give it anothe name in function
+             * function have acces the data 
+             * In passing by Reference it be (Input OutPut)
+             
+             */
+
+            #endregion
+
+
+         
+        }
+    }
+}
